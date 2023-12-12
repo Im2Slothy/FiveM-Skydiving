96cb2ccad490530fc472e3c16f5c1f55ff887bc2
Chris "Slothy <chrisbennett555@yahoo.com>
2023-12-11 19:39:06 -0600

    dadawdadd

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+--FiveM Skydive Script
+-- Version 1.0
+
+version '1.0.0'
+author 'Im2Slothy#0 & TonyVee#0'
+description 'Time to go skydiving!'
+repository 'https://github.com/Im2Slothy/FiveM-Skydiving'
+
+-- compatibility wrapper
+fx_version 'adamant'
+game 'common'
+
+-- Add a client script 
+client_script "client.lua"
+client_script "server.lua"
